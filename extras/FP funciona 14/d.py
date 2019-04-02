bd3b3a7c083b18ec243aab8991978e80af1b988e
Rafael Alexandre <RafaelAlexandreIST@github.com>
2019-04-02 22:21:12 +0100

    Update name of folders

--- extras/FP funciona 14/d.py
+++
@@ -1,8 +0,0 @@
-def digit(n):
-    if n == '':
-        return False
-    elif len(n) == 1:
-        return ('0' <= n <= '9')
-    else:
-        return ('0' <= n[0] <= '9') and digit(n[1:])
-print(digit('543+1')
